5e10c9562a0aa2b94b8ba7d270a5d46e63e1fd78
apacifico <pacifico.ang@gmail.com>
2011-09-10 12:42:48 +0200

    Integrate fluent mapping provided by schotime in petapoco using a template class supporting imapper interface. (I like the fluent syntax proposed by schotime)
    provide a rapide solution to support multiple database with different mapping for the same poco. (to support a concrete problem) . See mapperregistry class.
    
    Angelo

---
+++ PetaPoco/MapperRegistry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetaPoco
+{
+    public static class MapperRegistry
+    {
+        [ThreadStatic]
+        private static Database _current;
+
+        [ThreadStatic]
+        private static Dictionary<PetaPoco.Database, IMapper> registry = new Dictionary<Database, IMapper>();
+
+        public static void Register(PetaPoco.Database database, IMapper mapper)
+        {
+            if(!registry.ContainsKey(database)) registry.Add(database, mapper);
+        }
+
+        public static void Cleanup()
+        {
+            registry.Clear();
+            _current = null;
+        }
+
+        public static IMapper Mapper {
+        get {
+            if (!registry.ContainsKey(_current)) return null;
+            else return registry[_current];
+            }
+        }
+
+        public static IMapper LookupMapper(PetaPoco.Database database)
+        {
+            if (!registry.ContainsKey(database)) return null;
+            else return registry[database];
+        }
+
+        public static Database Current { 
+            set {
+                    _current = value; 
+                }
+        }
+    }
+}
